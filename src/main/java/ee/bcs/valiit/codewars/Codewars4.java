b9e85cf19ed5dfc2c2c2fe77535f3adedc39c3f9
Kaie Palmaru <kaiehe@yahoo.com>
2021-04-25 21:24:40 +0300

    Codewars test

---
+++ src/main/java/ee/bcs/valiit/codewars/Codewars4.java
@@ -0,0 +1,42 @@
+//package ee.bcs.valiit.codewars;
+//
+//import java.util.Arrays;
+//
+//public class Codewars4 {
+//
+////    public static void main(String[] args) {
+////        int arr[] = {2, 1, 10};
+////        sumOfDifferences(arr);
+////        System.out.println(sumOfDifferences(arr));
+////
+////    }
+//
+//    public static int sumOfDifferences(int[] arr) {
+////         int[] resultArray = new int[arr.length];
+////        int temp;
+////        int sum = 0;
+//        //       int difference;
+////        Arrays.sort(arr);
+////        if (arr.length <= 1) {
+////            return 0;
+////        }
+//
+////        for (int i = 0; i < arr.length; i++) {
+////            for (int j = i + 1; j < arr.length; j++) {
+////                if (arr[i] > arr[j]) {
+////                    int temp;
+////                    temp = arr[i];
+////                    arr[i] = arr[j];
+////                    arr[j] = temp;
+////
+////                }if (i!=arr.length-1)
+////                sum  += (arr[i] - arr[i+1]);
+////            }
+////
+////
+////        }
+////        return sum;
+//
+////}
+//
+//}
