4c50479cdef05c19c6789dd5908c003a9897ccc5
Kaie Palmaru <kaiehe@yahoo.com>
2021-04-27 15:52:28 +0300

    HTML, JS, CSS files renewal

--- src/main/resources/vue_sample/About.vue
+++ src/main/resources/vue_sample/About.vue
@@ -4,22 +4,24 @@
     <input v-model.number="minA">
     <input v-model.number="minB">
     <button v-on:click="min()">Min</button>
-    {{minAnswer}}
+    {{ minAnswer }}
+
   </div>
 </template>
 
 <script>
 
-function min(){
-  if(this.minA < this.minB){
+function min() {
+  if (this.minA < this.minB) {
     this.minAnswer = this.minA;
   } else {
     this.minAnswer = this.minB;
   }
 }
 
+
 export default {
-  data: function (){
+  data: function () {
     return {
       'minA': 0,
       'minB': 0,
